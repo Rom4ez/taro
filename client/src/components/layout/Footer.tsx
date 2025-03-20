3c5e1a75246eee53e97388e6df5dbe0e8ff7d101
Rom4ez <romap74@gmail.com>
2025-03-21 00:49:34 +0500

    Delete client/src directory

--- client/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="py-6 text-center text-sm text-gray-400">
-      <p>© {new Date().getFullYear()} Мистический Таро | Все права защищены</p>
-    </footer>
-  );
-}
